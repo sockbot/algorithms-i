19da39ac57dbe88c2442c8e2c3af2235f26c8347
Andy Chan <github@sockbot.com>
2020-04-18 12:38:50 -0700

    set up class according to spec

---
+++ queues/Deque.java
@@ -0,0 +1,45 @@
+/* *****************************************************************************
+ *  Name: Andrew Chan
+ *  Date: 18 Apr 2020
+ *  Description: Princeton Algorithms I, Queues
+ **************************************************************************** */
+
+public class Deque {
+
+    // construct an empty deque
+    public Deque() {
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+    }
+
+    // return the number of items on the deque
+    public int size() {
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+    }
+}
