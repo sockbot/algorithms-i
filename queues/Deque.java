1d31be16ec4ac1d22be498395b9fbf07a8886832
Andy Chan <github@sockbot.com>
2020-04-18 21:28:26 -0700

    refactor to use head and tail pointers in addLast, Iterator and resize

--- queues/Deque.java
+++ queues/Deque.java
@@ -10,7 +10,8 @@ import java.util.NoSuchElementException;
 public class Deque<Item> implements Iterable<Item> {
 
     private Item[] deque;
-    private int numItems = 0; // number of Items in deque
+    private int head = 0;
+    private int tail = 0;
 
     // construct an empty deque using arrays
     public Deque() {
@@ -25,19 +26,19 @@ public class Deque<Item> implements Iterable<Item> {
         System.out.printf("Deque length is %d\n", d.length());
         System.out.println("--------------------------------");
         System.out.println("Adding 100 to Deque");
-        d.addFirst(100);
+        d.addLast(100);
         System.out.printf("Deque is empty: %b\n", d.isEmpty());
         System.out.printf("%d items in Deque\n", d.size());
         System.out.printf("Deque length is %d\n", d.length());
         System.out.println("--------------------------------");
         System.out.println("Adding 200 to Deque");
-        d.addFirst(200);
+        d.addLast(200);
         System.out.printf("Deque is empty: %b\n", d.isEmpty());
         System.out.printf("%d items in Deque\n", d.size());
         System.out.printf("Deque length is %d\n", d.length());
         System.out.println("--------------------------------");
         System.out.println("Adding 300 to Deque");
-        d.addFirst(300);
+        d.addLast(300);
         System.out.printf("Deque is empty: %b\n", d.isEmpty());
         System.out.printf("%d items in Deque\n", d.size());
         System.out.printf("Deque length is %d\n", d.length());
@@ -58,22 +59,29 @@ public class Deque<Item> implements Iterable<Item> {
 
     // return the number of items on the deque
     public int size() {
-        return numItems;
-    }
-
-    // add the item to the front
-    public void addFirst(Item item) {
-        if (item == null)
-            throw new IllegalArgumentException();
-        if (numItems == deque.length)
-            resize(2 * deque.length); // double the size of the deque array
-        deque[numItems++] = item;
+        return tail - head;
     }
 
     // add the item to the back
     public void addLast(Item item) {
         if (item == null)
             throw new IllegalArgumentException();
+        if (size() == deque.length)
+            resize(2 * deque.length); // double the size of the deque array
+        deque[tail++] = item;
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        // copy size() if shrinking capacity, copy deque.length if growing capacity
+        int length = Math.min(size(), deque.length);
+        for (int i = 0; i < length; i++) {
+            int copyIndex = i + (length / 2); // start indexing with equal offset on both ends
+            int dequeIndex = (copyIndex + head)
+                    % length; // reindex the head and tail if they've wrapped around the index
+            copy[copyIndex] = deque[dequeIndex];
+        }
+        deque = copy;
     }
 
     // remove and return the item from the front
@@ -83,11 +91,6 @@ public class Deque<Item> implements Iterable<Item> {
         return null;
     }
 
-    // return an iterator over items in order from front to back
-    public Iterator<Item> iterator() {
-        return new ReverseArrayIterator();
-    }
-
     // remove and return the item from the back
     public Item removeLast() {
         if (isEmpty())
@@ -95,29 +98,37 @@ public class Deque<Item> implements Iterable<Item> {
         return null;
     }
 
+    // add the item to the front
+    public void addFirst(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException();
+        // if (size() == deque.length)
+        //     resize(2 * deque.length); // double the size of the deque array
+        // deque[head--] = item;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
     private class ReverseArrayIterator implements Iterator<Item> {
 
-        private int i = numItems;
+        private int m = head;
+        private int n = tail;
 
         public boolean hasNext() {
-            return i > 0;
+            return n > m;
         }
 
         public Item next() {
-            if (i-- < 0)
+            if (n-- < m)
                 throw new NoSuchElementException();
-            return deque[--i];
+            return deque[--n];
         }
 
         public void remove() {
             throw new UnsupportedOperationException();
         }
-    }
-
-    private void resize(int capacity) {
-        Item[] copy = (Item[]) new Object[capacity];
-        for (int i = 0; i < numItems; i++)
-            copy[i] = deque[i];
-        deque = copy;
     }
 }
