d00c3d85760c501fe9dfac57d7275dce0f83d5d4
Andy Chan <github@sockbot.com>
2020-06-06 21:40:18 -0700

    set up list, exceptions, and iterator

--- queues/Deque.java
+++ queues/Deque.java
@@ -5,8 +5,11 @@
  **************************************************************************** */
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
+
+    private Node first, last;
 
     // construct an empty deque
     public Deque() {
@@ -36,14 +39,48 @@ public class Deque<Item> implements Iterable<Item> {
 
     // remove and return the item from the front
     public Item removeFirst() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
     }
 
     // remove and return the item from the back
     public Item removeLast() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
     }
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    private class ListIterator implements Iterator<Item> {
+
+        private Node current = first;
+
+        public boolean hasNext() {
+            return this.current != null;
+        }
+
+        public Item next() {
+            if (!this.hasNext())
+                throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 
     // unit testing (required)
