66c2e8bcea2ea9bf70e554271653fcd6f35c7701
Andy Chan <github@sockbot.com>
2021-03-17 17:41:25 -0700

    scaffold FastCollinearPoints

---
+++ collinear/FastCollinearPoints.java
@@ -0,0 +1,13 @@
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    public int numberOfSegments() {
+        return -1;
+    }
+
+    public LineSegment[] segments() {
+        return null;
+    }
+}
